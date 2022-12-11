8f26b979c512dded510d44461aaaabeceb92a9bd
Joaquín Nicolás <25700773+cabraljoaquin99@users.noreply.github.com>
2022-12-10 21:08:23 -0300

    .

--- MODULO 4/4.3.1.6.py
+++ MODULO 4/4.3.1.6.py
@@ -15,16 +15,14 @@ def is_year_leap(year):
 	else:
 		return True
 def anio_mes(xanio_xmes):
-	
-
-test_data = [1900, 2000, 2016, 1987]
-test_results = [False, True, True, False]
-for i in range(len(test_data)):
-	yr = test_data[i]
-	print(yr,"-> ",end="")
-	result = is_year_leap(yr)
-	if result == test_results[i]:
-		print("OK")
-	else:
-		print("Fallido")
+	test_data = [1900, 2000, 2016, 1987]
+	test_results = [False, True, True, False]
+	for i in range(len(test_data)):
+		yr = test_data[i]
+		print(yr,"-> ",end="")
+		result = is_year_leap(yr)
+		if result == test_results[i]:
+			print("OK")
+		else:
+			print("Fallido")
 
