ae0194af57cf34f91d4e8010e8862c15c76a85de
Joaquín Nicolás <25700773+cabraljoaquin99@users.noreply.github.com>
2023-08-26 21:04:47 -0300

    .

---
+++ Lista002.py
@@ -0,0 +1,18 @@
+# Definir una lista por asignación con 5 enteros. Mostrar por pantalla solo los elementos con valor iguales o superiores a 7.
+
+xenteros = [20,6,11,58,105]
+z = 7
+w = 0
+while z >= xenteros(w):
+    z += xenteros(w)
+    print ("Los valores mayores o iguales a siete son: \n")
+    print (xenteros(w))
+'''
+xenteros = [20,6,11,58,105]
+mayores_a_siete = []
+for i in xenteros:
+    if i >= 7:
+        mayores_a_siete.append(i)
+print(mayores_a_siete)
+
+'''
