46dcb66ccd032978f2bca846676997561055ab9b
Joaquín Nicolás <25700773+cabraljoaquin99@users.noreply.github.com>
2023-09-02 18:24:29 -0300

    .

---
+++ MODULO 2/Joaquin_Cabral_2_5_1_2.py
@@ -0,0 +1,5 @@
+hrs = 2 #número de horas
+segundos = 3600 #número de segundos en una hora
+
+print("Horas: ", hrs) #imprime el numero de horas
+print("Segundos en Horas: ", hrs * segundos) #segundos en determinado numero de horas
