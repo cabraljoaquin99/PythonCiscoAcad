bbb1e8a9e1216945ace11594ca24719334ef94af
Joaquín Nicolás <25700773+cabraljoaquin99@users.noreply.github.com>
2023-09-02 18:53:02 -0300

    .

---
+++ MODULO 2/Joaquin_Cabral_2_6_1_9.py
@@ -0,0 +1,9 @@
+Numero_a = 6.1
+Numero_b = 5.2
+
+Suma = (print ("total de la suma: ", Numero_a+Numero_b))
+Resta = (print ("total de la resta: ", Numero_a-Numero_b))
+Multiplicacion = (print ("total de la multiplicación: ", Numero_a*Numero_b))
+Division = (print ("total de la división: ", Numero_a/Numero_b))
+
+print("\n¡Eso es todo, amigos!")
