cf8da2c5d024424045f35aaf986b0a35ee9614f1
Joaquín Nicolás <25700773+cabraljoaquin99@users.noreply.github.com>
2023-09-02 16:02:18 -0300

    .

--- MODULO 2/Joaquin_Cabral_2_4_1_9.py
+++ MODULO 2/Joaquin_Cabral_2_4_1_9.py
@@ -6,8 +6,8 @@
 kilometros = 12.25
 millas = 7.38
 
-millas_a_kilometros = ( 7.38 * 1.609) ### 'millas p/hr' multiplicar por 1.609
-kilometros_a_millas = (12.25 * 0.621) ###Km/h multiplicar por 0.621
+millas_a_kilometros = millas * 1.61
+kilometros_a_millas = kilometros / 1.61
 
 print(millas, "millas son", round(millas_a_kilometros, 2), "kilómetros")
 print(kilometros, "kilómetros son", round(kilometros_a_millas, 2), "millas")
