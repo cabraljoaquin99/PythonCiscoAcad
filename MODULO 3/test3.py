ea40b3b6be6af43dd654aa3f52dbf7cb6d1fc87f
Joaquín Nicolás <25700773+cabraljoaquin99@users.noreply.github.com>
2023-10-08 14:22:26 -0300

    .

--- MODULO 3/test3.py
+++ MODULO 3/test3.py
@@ -15,3 +15,26 @@ while swapped:
 
 print("\nOrdenada:")
 print(my_list)
+
+'''
+El algoritmo utilizado en este programa es el "algoritmo de ordenamiento de burbuja" (bubble sort en inglés). 
+Este algoritmo es uno de los métodos más sencillos de ordenamiento 
+y funciona comparando pares de elementos adyacentes en la lista y realizando intercambios si es necesario hasta que la 
+lista esté completamente ordenada.
+En el código que proporcionaste:
+Se crea una lista llamada my_list para almacenar los elementos que deseas ordenar.
+Luego, se solicita al usuario que ingrese la cantidad de elementos que desea ordenar (num) y se pide que ingrese esos 
+elementos uno por uno en la lista.
+Después, se entra en un bucle while que se ejecuta hasta que no haya más intercambios necesarios en la lista. 
+Inicialmente, swapped se establece en True para que el bucle se ejecute al menos una vez.
+Dentro del bucle while, se itera a través de la lista y se compara cada par de elementos adyacentes. 
+Si un elemento es mayor que el siguiente, se intercambian. Si se realiza un intercambio en algún momento, swapped 
+se establece en True, lo que significa que el bucle debe continuar para verificar nuevamente si se requieren más 
+intercambios.
+Cuando no se realizan intercambios en una pasada completa a través de la lista, swapped se establece en False, 
+lo que indica que la lista está completamente ordenada y el bucle while se detiene.
+Finalmente, se imprime la lista ordenada.
+El algoritmo de ordenamiento de burbuja puede no ser eficiente para listas muy grandes, pero es fácil de entender y es útil para 
+fines educativos. Para aplicaciones prácticas en listas grandes, otros algoritmos de ordenamiento como Quicksort o Mergesort suelen 
+ser más eficientes.
+'''
